8c25b74ebee56566df3d5f1685aa9cf5a2369954
Davashdgreat <109927389+Davashdgreat@users.noreply.github.com>
2023-10-01 21:28:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var typingFx = new Typed('.multxt', {
+    strings : ['Ashaolu David', 'A Front-end Developer' , 'A Back-end Developer'],
+    loop : true, 
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+})
